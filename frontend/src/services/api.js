62eb0f6b005303672b8e0845451cbf7f5e7ca355
SumantSingh08 <sumant082003@gmail.com>
2026-02-13 12:02:17 +0530

    updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://tasksphere-backend-m0dd.onrender.com/api",
 });
 
 API.interceptors.request.use((req) => {
